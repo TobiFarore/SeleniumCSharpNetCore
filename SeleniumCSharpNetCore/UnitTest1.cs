d73a83bceebd9617e9b16bec4abe3b8048f94539
TobiFarore <tobifarore@icloud.com>
2021-12-30 04:23:34 +0000

    removed first test

--- SeleniumCSharpNetCore/UnitTest1.cs
+++ SeleniumCSharpNetCore/UnitTest1.cs
@@ -21,20 +21,20 @@ namespace SeleniumCSharpNetCore
             Driver = new ChromeDriver(options);
         }
 
-        [Test]
-        public void Test1()
-        {
-            Driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com/");
-            Driver.FindElement(By.Id("ContentPlaceHolder1_Meal")).SendKeys("Tomato");
-            Driver.FindElement(By.XPath("//input[@name='ctl00$ContentPlaceHolder1$ChildMeal1']/following-sibling::div[text()='Broccoli']")).Submit();
+        //[Test]
+        //public void Test1()
+        //{
+        //    Driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com/");
+        //    Driver.FindElement(By.Id("ContentPlaceHolder1_Meal")).SendKeys("Tomato");
+        //    Driver.FindElement(By.XPath("//input[@name='ctl00$ContentPlaceHolder1$ChildMeal1']/following-sibling::div[text()='Broccoli']")).Submit();
 
-            //CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Apple");
+        //    //CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Apple");
 
-            //var dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location).ToString();
-            //var screenshot = (Driver as ITakesScreenshot).GetScreenshot();
-            //screenshot.SaveAsFile($"{dir}\\screenshot.png");
-            //Assert.Pass();
-        }
+        //    //var dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location).ToString();
+        //    //var screenshot = (Driver as ITakesScreenshot).GetScreenshot();
+        //    //screenshot.SaveAsFile($"{dir}\\screenshot.png");
+        //    //Assert.Pass();
+        //}
 
         //[Test]
         //public void LoginTest()
