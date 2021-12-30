f0008d7ba028d628d11751d88673abe802374119
TobiFarore <tobifarore@icloud.com>
2021-12-30 00:51:58 +0000

    s

---
+++ SeleniumCSharpNetCore/UnitTest1.cs
@@ -0,0 +1,36 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace SeleniumCSharpNetCore
+{
+    public class Tests : DriverHelper
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Console.WriteLine("SetUp");
+            Driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com/");
+            Driver.FindElement(By.Id("ContentPlaceHolder1_Meal")).SendKeys("Tomato");
+            Driver.FindElement(By.XPath("//input[@name='ctl00$ContentPlaceHolder1$ChildMeal1']/following-sibling::div[text()='Broccoli']")).Submit();
+
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Apple");
+            
+            Console.WriteLine("Test1");
+            Assert.Pass();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.Quit();
+        }
+    }
+}
